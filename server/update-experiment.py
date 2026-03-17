2a22cad988b198b195196b806564debc62c3a26c
Geoffrey Ottoy <geoffrey.ottoy@kuleuven.be>
2026-03-17 16:08:05 +0100

    Update update-experiment.py

--- server/update-experiment.py
+++ server/update-experiment.py
@@ -64,6 +64,7 @@ print("Working on", len(host_list) ,"tile(s):", tiles)
 
 # First we test connectivity
 nr_active_tiles = 0
+all_failed_tiles = []
 if test_connectivity:
     print("Testing connectivity ... ")
     playbook_path = os.path.join(config.PLAYBOOK_DIR, "ping.yaml")
@@ -82,6 +83,7 @@ if test_connectivity:
     if not (nr_active_tiles == len(host_list)):
         print("Unable to connect to all tiles.")
         print("Inactive tiles:", failed_tiles)
+        all_failed_tiles.append(failed_tiles)
         if halt_on_connectivity_failure:
             print("Aborting (halt_on_connectivity_failure = True)")
             sys.exit(config.ERRORS["CONNECTIVITY_ERROR"])
@@ -109,9 +111,10 @@ playbook_path = os.path.join(config.PLAYBOOK_DIR, "manage-service.yaml")
     cleanup=True
 )
 
-if not (nr_active_tiles == len(host_list)):
+if not (nr_active_tiles == prev_nr_active_tiles):
     print("Unable to connect to all tiles.")
     print("Inactive tiles:", failed_tiles)
+    all_failed_tiles.append(failed_tiles)
     if halt_on_connectivity_failure:
         print("Aborting (halt_on_connectivity_failure = True)")
         sys.exit(config.ERRORS["CONNECTIVITY_ERROR"])
@@ -139,9 +142,10 @@ playbook_path = os.path.join(config.PLAYBOOK_DIR, "pull-repo.yaml")
     cleanup=True
 )
 
-if not (nr_active_tiles == len(host_list)):
+if not (nr_active_tiles == prev_nr_active_tiles):
     print("Unable to connect to all tiles.")
     print("Inactive tiles:", failed_tiles)
+    all_failed_tiles.append(failed_tiles)
     if halt_on_connectivity_failure:
         print("Aborting (halt_on_connectivity_failure = True)")
         sys.exit(config.ERRORS["CONNECTIVITY_ERROR"])
@@ -169,9 +173,10 @@ playbook_path = os.path.join(config.PLAYBOOK_DIR, "run-script.yaml")
     cleanup=True
 )
 
-if not (nr_active_tiles == len(host_list)):
+if not (nr_active_tiles == prev_nr_active_tiles):
     print("Unable to connect to all tiles.")
     print("Inactive tiles:", failed_tiles)
+    all_failed_tiles.append(failed_tiles)
     if halt_on_connectivity_failure:
         print("Aborting (halt_on_connectivity_failure = True)")
         sys.exit(config.ERRORS["CONNECTIVITY_ERROR"])
@@ -179,5 +184,10 @@ if not (nr_active_tiles == len(host_list)):
         print("Proceeding with", nr_active_tiles, "tiles(s):", tiles)
 
 print("Updated experiment client script on tiles(s):", tiles)
+
+failed_summary = " ".join(all_failed_tiles)
+if failed_summary:
+    print("Not all operations were succesful on every tile.")
+    print("Check tiles:", failed_summary)
 
 print("Done.")
